74161f88d6feede9ff3d7cbafa41bcf6862bcad7
Anthony Boccadoro <aboccadoro@umass.edu>
2015-10-24 19:21:41 -0400

    everything coded

--- src/main/scala/vm/MyVirtualMachine.scala
+++ src/main/scala/vm/MyVirtualMachine.scala
@@ -1,16 +1,24 @@
 package vm
 
-import bc.ByteCode
+import bc.{ByteCodeValues, ByteCode}
 
 /**
 *  Created by Anthony on 10/22/2015.
 */
-class MyVirtualMachine extends VirtualMachine {
+class MyVirtualMachine extends VirtualMachine with ByteCodeValues {
   val stack = Vector[Int]()
-  def execute(bc: Vector[ByteCode]): VirtualMachine = ???
-  // TODO
-  def executeOne(bc: Vector[ByteCode]): (Vector[ByteCode], VirtualMachine) = ???
-  // TODO
+  def execute(bc: Vector[ByteCode]): VirtualMachine = {
+    for(code <- bc) {
+      code.execute(this)
+    }
+    this
+    // TODO
+  }
+  def executeOne(bc: Vector[ByteCode]): (Vector[ByteCode], VirtualMachine) = {
+    bc.head.execute(this)
+    (bc.tail, this)
+    // TODO
+  }
   def push(value: Int): VirtualMachine = {
     stack :+ value
     this
@@ -18,7 +26,7 @@ class MyVirtualMachine extends VirtualMachine {
   }
   def pop(): (Int, VirtualMachine) = {
     if (stack.isEmpty) throw new MachineUnderflowException("no more elements left to pop()")
-    val value = stack(0)
+    val value = stack.head
     stack.drop(1)
     (value, this)
     // TODO
