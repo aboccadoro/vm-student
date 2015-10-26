5f6b09c4ea7f59252cdc39716cb060480459755a
Anthony Boccadoro <aboccadoro@umass.edu>
2015-10-25 22:25:18 -0400

    all tests passed

--- src/main/scala/vendor/VendorProgramParser.scala
+++ src/main/scala/vendor/VendorProgramParser.scala
@@ -1,6 +1,6 @@
 package vendor
 
-import bc.ByteCodeValues
+import bc.{InvalidBytecodeException, ByteCodeValues}
 
 /**
 *  Created by Anthony on 10/22/2015.
@@ -14,7 +14,7 @@ class VendorProgramParser extends ProgramParser with ByteCodeValues {
     var instructions: InstructionList = Vector[Instruction]()
     val order = string.split("\n")
     for (s <- order) {
-      if (!names.contains(s.split(" ").head) && !Character.isDigit(s.last)) throw new InvalidInstructionFormatException(s + " not a valid instruction input")
+      if (!names.contains(s.split(" ").head) && !Character.isDigit(s.last)) throw new InvalidBytecodeException(s + " not a valid ByteCode")
       else if (s.split(" ").head.equals("iconst")) instructions = instructions :+ new Instruction(s.split(" ").head, Vector[Int](s.split(" ").last.toInt))
       else if (names.contains(s)) instructions = instructions :+ new Instruction(s, Vector[Int]())
     }
